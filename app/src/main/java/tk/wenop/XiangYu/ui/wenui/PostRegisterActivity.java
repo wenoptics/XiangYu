fcd115d6f31df8a81de749867314d9140c1ba6ce
wenop <wenoptics@163.com>
2015-11-03 23:38:29 +0800

    update 2种注册页面;

---
+++ app/src/main/java/tk/wenop/XiangYu/ui/wenui/PostRegisterActivity.java
@@ -0,0 +1,15 @@
+package tk.wenop.XiangYu.ui.wenui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import tk.wenop.XiangYu.R;
+
+public class PostRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_register);
+    }
+}
